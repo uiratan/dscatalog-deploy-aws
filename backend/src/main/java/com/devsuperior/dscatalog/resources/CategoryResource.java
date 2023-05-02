fbc22f3fa8273d2ae10c25944066cd9dd2a490c7
uiratan <uiratan@gmail.com>
2023-05-02 20:00:17 -0300

    Category DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,11 +20,14 @@ public class CategoryResource {
 	private CategoryService categoryService;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = categoryService.findAll();
-//		list.add(new Category(1L, "Books"));
-//		list.add(new Category(2L, "Eletronics"));
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = categoryService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = categoryService.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRespository;
 
@@ -16,8 +19,17 @@ public class CategoryService {
 	private CategoryRespository categoryRespository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return categoryRespository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = categoryRespository.findAll();
+		return list.stream().map(cat -> new CategoryDTO(cat)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		 Optional<Category> obj = categoryRespository.findById(id);
+		 Category entity = obj.get();
+		 return new CategoryDTO(entity);
+		 
 	}
 
 }
