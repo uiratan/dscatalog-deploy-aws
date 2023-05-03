9f4097b3d18932908f08fb3809feb732ef1233ae
uiratan <uiratan@gmail.com>
2023-05-02 21:05:31 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -33,4 +33,12 @@ public class CategoryService {
 		 
 	}
 
+	@Transactional(readOnly = true)
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = categoryRespository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
 }
